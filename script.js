806ad4fdcfb18a616ce1f6fda49eec33c9838300
Kenneth Buan <buancneth@gmail.com>
2019-07-13 01:52:39 +0800

    fix contact form

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const getInTouchBtn = document.querySelector("#get-in-touch"); 
 const contactContainer = document.querySelector(".contact-container"); 
+const contactClose = document.querySelector(".close"); 
 
 getInTouchBtn.addEventListener("click", () => {
+    contactContainer.classList.toggle("enable");
+});
+
+contactClose.addEventListener("click", () => {
     contactContainer.classList.toggle("enable");
 });
